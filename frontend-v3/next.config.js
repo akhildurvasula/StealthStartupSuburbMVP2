1656b4d66631ef38252567d8e7f5e3f4374dee90
Akhil Durvasula <akhildurvasula@akhils-mbp.wireless-1x.unc.edu>
2025-12-10 15:42:57 -0500

    Fix: Configure Next.js to allow Unsplash images

--- frontend-v3/next.config.js
+++ frontend-v3/next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+    unoptimized: true, // Allow unoptimized images for external URLs
+  },
 }
 
 module.exports = nextConfig
-
